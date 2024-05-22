1c50f474b3fe79062b48c76fa3b728721d291d96
JoshuaNderitu <137731183+JoshuaNderitu@users.noreply.github.com>
2024-05-22 11:46:20 +0300

    initialializing the psuedo code

---
+++ numberguessing.py
@@ -0,0 +1,18 @@
+import random
+import math
+
+#taking inputs
+
+#generate random number between upper bound and lower bound 
+
+
+#initialize the number of guesses to zero
+
+#initialize a while loop
+#minimum number of guesses depends upon range
+
+
+#taking guessing number as input
+
+#testing the condition
+
